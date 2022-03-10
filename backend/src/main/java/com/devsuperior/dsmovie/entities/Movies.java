570e07959bdf3f961cb3b21c658af545c1b14e83
aleciquielo <aciquielo.dev@gmail.com>
2022-03-10 20:25:37 -0300

    Save score

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movies.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movies.java
@@ -1,9 +1,13 @@
 package com.devsuperior.dsmovie.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +21,9 @@ public class Movies {
 	private Double score;
 	private Integer count;
 	private String image;
+	
+	@OneToMany(mappedBy = "id.movie")
+	private Set<Score> scores = new HashSet<>();
 	
 	public Movies() {
 	}
@@ -67,7 +74,9 @@ public class Movies {
 
 	public void setImage(String image) {
 		this.image = image;
-	}	
+	}
 
-	
+	public Set<Score> getScores() {
+		return scores;
+	}	
 }
